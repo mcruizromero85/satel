75d1248eda85d3b2cb2878ae077eb5facf985219
Mauro <mcruizromero85@gmail.com>
2014-06-28 21:38:39 -0500

    Regularizando archivos faltantes

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require_relative '../lib/edr'
 
 # Initialize the Rails application.
 Satel::Application.initialize!
 
-Capybara.app_host = 'http://192.168.56.1:3000/'
+#Capybara.app_host = 'http://192.168.56.1:3000/'
 
-Edr::Registry.define do
-  map Torneo, TorneoData
-end
