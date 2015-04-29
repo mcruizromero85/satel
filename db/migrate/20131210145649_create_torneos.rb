b94fe58736b7c73eecd93d2530051e6173fc155d
Mauro <mcruizromero85@gmail.com>
2015-04-28 22:47:50 -0500

    Prueba migrate

--- db/migrate/20131210145649_create_torneos.rb
+++ db/migrate/20131210145649_create_torneos.rb
@@ -1,19 +1,19 @@
 class CreateTorneos < ActiveRecord::Migration
   def change
     create_table :torneos do |t|
-      t.string :titulo
-      t.string :paginaweb
-      t.integer :vacantes
-      t.timestamp :cierre_inscripcion_fecha
-      t.timestamp :cierre_inscripcion_tiempo
-      t.integer :periodo_confirmacion_en_minutos
-      t.string :tipo_torneo
-      t.string :tipo_generacion	
-      t.integer :gamer_id
-      t.integer :juego_id
-      t.foreign_key :juegos
-      t.string :estado
-      t.timestamps
+            t.string   "titulo", null: false
+            t.string   "post_detalle_torneo"
+            t.string   "urlstreeming"
+            t.integer  "vacantes", null: false
+            t.datetime "cierre_inscripcion", null: false
+            t.integer  "periodo_confirmacion_en_minutos", null: false
+            t.string   "tipo_torneo"# Individual / Moba
+            t.string   "tipo_generacion" #Simple / Doble / Grupos Round Robin / Grupos Round Robin doble llave
+            t.integer  "gamer_id"
+            t.integer  "juego_id"
+            t.string   "estado"
+            t.datetime "created_at"
+            t.datetime "updated_at"
     end
   end
 end
