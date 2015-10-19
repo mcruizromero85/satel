46e9a883da4fe011109f7aa0b4d3aa37346fa4a5
Mauro <mcruizromero85@gmail.com>
2015-10-18 21:24:17 -0500

    Mejoras a la nueva portada, con angular

---
+++ app/assets/javascripts/torneo.js
@@ -0,0 +1,31 @@
+var torneoApp = angular.module('torneoApp', []);
+
+torneoApp.controller('TorneoController', function ($scope, $http){
+  $http.get('ultimo_torneo_finalizado.json').success(function(data) {
+    $scope.ultimo_torneo_finalizado = data.detail;
+    $scope.inscripciones = data.inscripciones;
+    $scope.minimalData = {
+        teams : data.teams,
+        results: data.results
+      }
+  });  
+});
+
+
+$.ajax({
+  url: 'ultimo_torneo_finalizado.json',
+  data: {
+    format: 'json'
+  },
+  error: function() {
+    console.log('Se jodio');
+  },
+  success: function(data) {
+    var minimalData = {
+      teams : data.teams,
+      results: data.results
+    }
+    $('#llaves').bracket({init: minimalData, skipConsolationRound: true });
+    console.log(data); 
+  }
+});
